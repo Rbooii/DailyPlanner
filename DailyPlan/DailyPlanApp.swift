07c9923543c226343bd0df50e3034ba32b19e52c
Rbooii <arconumberone@gmail.com>
2026-03-25 16:20:14 +0700

    Initial Commit

---
+++ DailyPlan/DailyPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyPlanApp.swift
+//  DailyPlan
+//
+//  Created by Arco zakwan putra on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
